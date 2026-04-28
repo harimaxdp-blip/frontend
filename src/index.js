dcdbff72741477f0b4ab04b52cc7ee7013a13bc4
harimaxdp-blip <harimaxdp@gmail.com>
2026-04-28 12:19:05 +0530

    fix vercel routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
+// IMPORTANT: HashRouter fixes refresh + Vercel blank page issues
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
